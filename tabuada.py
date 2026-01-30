0a23428200e40e8fecc02ac4b5706f01a7d69713
Henderson Silva Barreto <silvabarretohenderson@gmail.com>
2026-01-30 18:42:47 -0300

    tabuada-do-henderson

---
+++ tabuada.py
@@ -0,0 +1,33 @@
+import os
+
+#limpar terminal sem me atrapalhar:
+def limpar():
+    os.system('cls' if os.name == 'nt' else 'clear')
+try:
+    print('\n   Olá! Seja muito bem-vindo/a a tabuada do Henderson!')
+
+    print('\n1 - Iniciar')
+    print('2 - Sair\n')
+
+    escolha = int(input('Digite a sua escolha:  '))
+
+    if escolha == 1:
+        numero = int(input('Número para iniciar a operação: '))
+
+        for i in range(0,11):
+            print('\nO resultado de', numero, 'X', i, '=', i * numero)
+
+        print('\n\nPrograma finalizado com sucesso!')
+        print('Até mais!')
+        input('\nTecle ENTER para limpar terminal')
+
+        limpar()
+    elif escolha == 2:
+        limpar()
+        print('Até mais!')
+    else:
+        limpar()
+        print('Escolha um número entra 1 e 2!')
+except ValueError:
+    limpar()
+    print('Opção invalida!')
